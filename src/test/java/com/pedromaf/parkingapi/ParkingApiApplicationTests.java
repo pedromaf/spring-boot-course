417604c91da423dd0a322778a1588a61e883d1ba
Pedro Marinho <pmaf@ic.ufal.br>
2024-04-30 17:11:57 -0300

    Initial project files.

---
+++ src/test/java/com/pedromaf/parkingapi/ParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromaf.parkingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
